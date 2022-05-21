09ae3b1e589a7d7afa74caa7e9e3dfa43156849f
Caio Moraes <caio.moraes.fbp@gmail.com>
2022-05-21 11:33:21 -0300

    tests

--- converter.js
+++ converter.js
@@ -1,58 +1,58 @@
+// class Converter {
+//     constructor() {}
 
-// conversor hexa para rgb
-// function hexToRGB(hex, alpha) {
-//     var r = parseInt(hex.slice(1, 3), 16),
-//         g = parseInt(hex.slice(3, 5), 16),
-//         b = parseInt(hex.slice(5, 7), 16);
+//     hexToRGB(hex, alpha) {
+//         var r = parseInt(hex.slice(1, 3), 16),
+//             g = parseInt(hex.slice(3, 5), 16),
+//             b = parseInt(hex.slice(5, 7), 16);
+    
+//         if (alpha) {
+//             return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+//         } else {
+//             return "rgb(" + r + ", " + g + ", " + b + ")";
+//         }
+//     }
 
-//     if (alpha) {
-//         return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
-//     } else {
-//         return "rgb(" + r + ", " + g + ", " + b + ")";
+//     ColorToHex(color) {
+//         var hexadecimal = color.toString(16);
+//         return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+//     }
+
+//     ConvertRGBtoHex(red, green, blue) {
+//         return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
 //     }
 // }
+
+
+// module.exports = Converter
+
+// conversor hexa para rgb
+function hexToRGB(hex, alpha) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+}
 
 // console.log(hexToRGB('#FF0000', 0.5)); 
 // console.log(hexToRGB('#c3c3c3')); 
 // console.log()
 
 // conversor rgb para hexa
-// function ColorToHex(color) {
-//     var hexadecimal = color.toString(16);
-//     return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
-// }
+function ColorToHex(color) {
+    var hexadecimal = color.toString(16);
+    return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+}
   
-// function ConvertRGBtoHex(red, green, blue) {
-//     return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
-// }
-
-// console.log(ConvertRGBtoHex(27, 100, 200));
-
-// module.exports = {hexToRGB, ConvertRGBtoHex}
-class Converter {
-    constructor() {}
-
-    hexToRGB(hex, alpha) {
-        var r = parseInt(hex.slice(1, 3), 16),
-            g = parseInt(hex.slice(3, 5), 16),
-            b = parseInt(hex.slice(5, 7), 16);
-    
-        if (alpha) {
-            return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
-        } else {
-            return "rgb(" + r + ", " + g + ", " + b + ")";
-        }
-    }
-
-    ColorToHex(color) {
-        var hexadecimal = color.toString(16);
-        return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
-    }
-
-    ConvertRGBtoHex(red, green, blue) {
-        return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
-    }
+function RGBtoHex(red, green, blue) {
+    return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
 }
 
+// console.log(RGBtoHex(27, 100, 200));
 
-module.exports = Converter
+module.exports = {hexToRGB, RGBtoHex}
--- tests/converter.test.js
+++ tests/converter.test.js
@@ -1,1 +1,27 @@
-const chain = require('chain');
+const { assert } = require('chai')
+
+const Converter = require("../converter")
+
+describe("Convertendo cores hexadecimal para rgb", () => {
+    
+    it("Sucesso ao converter rgb", () => {
+        const valorEsperado = "rgb(255, 255, 255)"
+        const resultado = Converter.hexToRGB("#FFFFFF")
+        assert.equal(resultado, valorEsperado)
+    })
+
+    it("Sucesso ao converter rgba", () => {
+        const valorEsperado = "rgba(255, 255, 255, 0.5)"
+        const resultado = Converter.hexToRGB("#FFFFFF", 0.5)
+        assert.equal(resultado, valorEsperado)
+    })
+
+})
+
+describe("Convertendo cores rgb para hexadecimal", () => {
+    it("Sucesso ao converter hexadecimal", () => {
+        const valorEsperado = "#000000"
+        const resultado = Converter.RGBtoHex(0, 0, 0)
+        assert.equal(resultado, valorEsperado)
+    })
+})
